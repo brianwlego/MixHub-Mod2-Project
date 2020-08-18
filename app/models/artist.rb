05ab9796d9e5b8c117533d64f8f760bd91f47c7b
brianwlego <legobw@gmail.com>
2020-08-17 23:41:54 -0400

    basic form working

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,10 +2,5 @@ class Artist < ApplicationRecord
     has_many :tracklists 
     has_many :songs, through: :tracklists
 
-    def artist_attributes=(artist_attributes)
-        byebug
-        artist_attributes.values.each do |artist_attribute|
-            artist = Artist.find_or_create_by(name: artist_attribute[:name])
-        end
-    end
+    
 end
