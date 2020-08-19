1589ca4029398af9655d8b38b3b3efa658352b63
Ryan Flynn <ryanflynn44@gmail.com>
2020-08-18 21:58:22 -0400

    Finsihed Sign Up Styling

--- app/models/tracklist.rb
+++ app/models/tracklist.rb
@@ -5,12 +5,20 @@ class Tracklist < ApplicationRecord
   has_many :songs
 
   def artist_attributes=(artist_attributes)  
-    artist = Artist.find_or_create_by(name: artist_attributes[:name])
+    if Artist.find_by(name: artist_attributes[:name])
+      artist = Artist.find_by(name: artist_attributes[:name])
+    else
+      artist = Artist.create(name: artist_attributes[:name], img_url: artist_attributes[:img_url])
+    end 
     self.artist = artist
   end
 
   def festival_attributes=(festival_attributes)
-        festival = Festival.find_or_create_by(name: festival_attributes[:name])
+    if Festival.find_by(name: festival_attributes[:name])
+      festival = Festival.find_by(name: festival_attributes[:name])  
+    else
+      festival = Festival.create(name: festival_attributes[:name], location: festival_attributes[:location], date: festival_attributes[:date])
+    end
     self.festival = festival
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Festival.delete_all
+Artist.delete_all
+Tracklist.delete_all
+
 
 #Tracklist Test
-f = Festival.create(name: "Ultra Miami", location: "Miami", date:2009-03-18)
-f2 = Festival.create(name: "Coachella", location: "The Deseret", date:2009-10-18)
+f = Festival.create(name: "Ultra Miami", location: "Miami", date:"2009-03-18")
+f2 = Festival.create(name: "Coachella", location: "The Deseret", date:"2009-10-18")
 
-a = Artist.create(name: "Beyonce", img_url: "laksjdflskjf")
-a2 = Artist.create(name: "Adele", img_url: "laksjdfasdflskjf")
-a3 = Artist.create(name: "Whitney Houston", img_url: "laksjsdfsdflskjf")
+a = Artist.create(name: "Beyonce", img_url: "https://pyxis.nymag.com/v1/imgs/6c0/570/056961772f9239c7ad26e784b020e410ae-beyonce.rsquare.w1200.jpg")
+a2 = Artist.create(name: "Adele", img_url: "https://www.nme.com/wp-content/uploads/2020/01/2020_adele_getty_2000x1270.jpg")
+a3 = Artist.create(name: "Whitney Houston", img_url: "https://i.pinimg.com/564x/f6/5e/30/f65e30f5157bedf02daa416ad4966f60.jpg")
 
 t = Tracklist.create(name: 'Tracklist 1', artist_id: 1, festival_id: 1, user_id: 2, link: "blahblah", img_url: "blahblahhh", likes: 0)
