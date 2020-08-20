2a8427dd75d9489e1abf82a2041e6dc7b82ae603
brianwlego <legobw@gmail.com>
2020-08-19 22:16:36 -0400

    started styling

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,7 @@ class Song < ApplicationRecord
   validates :title, :tracklist_number, :start_time, :artist_name,   presence: true
   validates :start_time, format: { without: /\A[a-zA-Z]+\z/,
     message: "only allows numbers" }
-  validates :tracklist_number, :start_time, uniqueness: true
+  validates :tracklist_number, :start_time, uniqueness: {scope: :tracklist_id}
 
   def verified?(user)
     !!self.verifications.find{|verification| verification.user_id == user.id}
