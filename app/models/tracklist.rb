643f289ae86a666ff448045337d8cb5d2c72686d
brianwlego <legobw@gmail.com>
2020-08-20 17:54:47 -0400

    Site Works

--- app/models/tracklist.rb
+++ app/models/tracklist.rb
@@ -3,8 +3,8 @@ class Tracklist < ApplicationRecord
   belongs_to :artist
   belongs_to :festival
   belongs_to :user
-  has_many :songs
-  has_many :likes
+  has_many :songs, dependent: :delete_all
+  has_many :likes, dependent: :delete_all
   validates :name, :img_url, :user_id, presence: true
   validates :name, uniqueness: true
   validates_associated :artist, :festival
@@ -33,8 +33,14 @@ class Tracklist < ApplicationRecord
   end
 
   def order_by_tracklist_number
-    self.songs.sort_by {|song| song.tracklist_number}
+      self.songs.sort_by {|song| song.tracklist_number}
   end
+
+  def first_three_songs
+    self.order_by_tracklist_number.first(3)
+  end
+
+  
   
   # if self.likes.include?(current_user) && @likes.include?(@tracklist)
 
